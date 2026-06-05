c566af1c7c903e4e71ea83277d5fcede22fee863
2610815-ops <2610815@sh.re.kr>
2026-06-05 05:46:26 +0000

    ㅇ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
-import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import streamlit as str
+popcorn = str.selectbox("팝콘 선택", ["기본", "카라멜", "어니언"])
+drink = str.selectbox("음료 선택", ["생수", "탄산음료"])
+str.write(f"🍿 선택하신 메뉴: 세트메뉴 {popcorn} 팝콘 + {drink}")
